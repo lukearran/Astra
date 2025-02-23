02ce4ca7040e15407b66bba3c6269379bdb45c1a
Luke Arran <hello@lukearran.com>
2025-02-23 13:59:21 +0000

    Set like, reply and repost counts on the content

--- Astra/Astra.Gtk/Views/StatusItem.cs
+++ Astra/Astra.Gtk/Views/StatusItem.cs
@@ -11,6 +11,8 @@ namespace Astra.Gtk.Views;
 
 public class StatusItem : ListBoxRow
 {
+    // Content model
+    private readonly PostView _content;
     // Posted By, Handles, and Posted At
     [global::Gtk.Connect("post_handle")] private readonly global::Gtk.Label? _postHandle = null;
     [global::Gtk.Connect("post_handle_sub")] private readonly global::Gtk.Label? _postHandleSub = null;
@@ -19,12 +21,20 @@ public class StatusItem : ListBoxRow
     [global::Gtk.Connect("post_content")] private readonly global::Gtk.Label? _postContent = null;
     // Buttons
     [global::Gtk.Connect("reply_button")] private readonly Button? _replyButton = null;
+    [global::Gtk.Connect("reply_button_content")] private readonly Adw.ButtonContent? _replyButtonContent = null;
+    
     [global::Gtk.Connect("repost_button")] private readonly ToggleButton? _repostButton = null;
+    [global::Gtk.Connect("repost_button_content")] private readonly Adw.ButtonContent? _repostButtonContent = null;
+    
     [global::Gtk.Connect("heart_button")] private readonly ToggleButton? _heartButton = null;
+    [global::Gtk.Connect("heart_button_content")] private readonly Adw.ButtonContent? _heartButtonContent = null;
 
     private StatusItem(Builder builder, PostView content) : base(new ListBoxRowHandle(builder.GetPointer("_root"), false))
     {
         builder.Connect(this);
+        
+        // Set models
+        _content = content;
         
         // Set the handle label
         _postHandle?.SetVisible(string.IsNullOrEmpty(content.Author.DisplayName) is false);
@@ -40,16 +50,19 @@ public class StatusItem : ListBoxRow
         _postContent?.SetText(content.PostRecord?.Text ?? string.Empty);
         
         // Register "Reply" button click event
+        _replyButtonContent?.SetLabel(content.ReplyCount.ToString() ?? "0");
         if (_replyButton != null)
         {
             _replyButton.OnClicked += ReplyButtonOnClicked;
         }
         // Register "Repost" button toggle event
+        _repostButtonContent?.SetLabel(content.RepostCount.ToString() ?? "0");
         if (_repostButton != null)
         {
             _repostButton.OnToggled += RepostButtonOnToggled;
         }
         // Register "Heart" (aka like/favorite) button toggle event
+        _heartButtonContent?.SetLabel(content.LikeCount.ToString() ?? "0");
         if (_heartButton != null)
         {
             _heartButton.OnToggled += HeartButtonOnToggled;
